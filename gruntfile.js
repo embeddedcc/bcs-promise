cfaa12d11d8740b4fcb6c668100ab4ca04c5ffbd
Brent Rahn <brent.rahn@gmail.com>
2014-07-12 12:04:28 -0700

    add grunt-contrib-copy

--- gruntfile.js
+++ gruntfile.js
@@ -64,6 +64,13 @@ grunt.initConfig({
 		tasks: 'default'
 	},
 	
+	copy: {
+		main: {
+			src: 'vendor/js/request.js',
+			dest: 'site/dist/request.js'
+		}
+	},
+	
 	// See https://github.com/thanpolas/grunt-github-pages for pre-requisites
 	githubPages: {
 		target: {
@@ -93,13 +100,14 @@ grunt.initConfig({
 grunt.loadNpmTasks('grunt-contrib-jshint');
 grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-copy');
 grunt.loadNpmTasks('grunt-github-pages');
 grunt.loadNpmTasks('grunt-jsdoc');
 grunt.loadNpmTasks('grunt-mocha');
 
 // Tasks (command line)
-grunt.registerTask('default', ['jshint', 'jsdoc']);
-grunt.registerTask('dist', ['jshint', 'uglify']);
+grunt.registerTask('default', ['jshint', 'jsdoc', 'copy']);
+grunt.registerTask('dist', ['jshint', 'uglify', 'copy']);
 // create an alias for the githubPages task
-grunt.registerTask('gh-pages', ['jshint', 'uglify', 'githubPages:target']);
+grunt.registerTask('gh-pages', ['jshint', 'uglify', 'copy', 'githubPages:target']);
 };
